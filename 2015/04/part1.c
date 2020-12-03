ba47f10725239c45db925dd3a92067af21f0c66f
Egor <egor@opensrc.club>
2020-12-03 17:36:50 +0200

    2015/04

---
+++ 2015/04/part1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include <openssl/md5.h>
+int main() {
+	const char *key = "ckczppom";
+	char buf[512];
+	unsigned char digest[MD5_DIGEST_LENGTH];
+	int i = 1;
+	for (;;) {
+		snprintf(buf, 512, "%s%d", key, i++);
+		MD5((unsigned char*)buf, strlen(buf), digest);
+		if (!digest[0] && !digest[1] && !(0xF0&digest[2])) {
+			printf("%d\n", i-1);
+			break;
+		}
+	}
+}
