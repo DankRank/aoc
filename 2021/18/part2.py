d8547c05034ae6dacd80c19517ccd395948fca24
Egor <egor@opensrc.club>
2021-12-18 08:25:45 +0200

    2021/18

---
+++ 2021/18/part2.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python3
+class Pair:
+    def __init__(self, lhs, rhs):
+        self.lhs = lhs
+        self.rhs = rhs
+        self.val = None
+        self.parent = None
+        lhs.parent = self
+        rhs.parent = self
+    def __repr__(self):
+        return '[%s,%s]' % (self.lhs, self.rhs)
+class Number:
+    def __init__(self, val):
+        self.lhs = None
+        self.rhs = None
+        self.val = val
+        self.parent = None
+    def __repr__(self):
+        return repr(self.val)
+def pred(node):
+    while node.parent is not None:
+        if node.parent.rhs is node:
+            node = node.parent.lhs
+            while node.rhs is not None:
+                node = node.rhs
+            return node
+        node = node.parent
+    return None
+def succ(node):
+    while node.parent is not None:
+        if node.parent.lhs is node:
+            node = node.parent.rhs
+            while node.lhs is not None:
+                node = node.lhs
+            return node
+        node = node.parent
+    return None
+def replacenode(node, newnode):
+    newnode.parent = node.parent
+    # won't work for root node
+    if node.parent.lhs is node:
+        node.parent.lhs = newnode
+    else:
+        node.parent.rhs = newnode
+class ReduceAgainException(Exception):
+    pass
+def explode(node, level=0):
+    if isinstance(node, Pair):
+        if level == 4:
+            p = pred(node)
+            s = succ(node)
+            if p is not None:
+                p.val += node.lhs.val
+            if s is not None:
+                s.val += node.rhs.val
+            replacenode(node, Number(0))
+            raise ReduceAgainException()
+        else:
+            explode(node.lhs, level+1)
+            explode(node.rhs, level+1)
+def split(node):
+    if isinstance(node, Number):
+        if node.val > 9:
+            replacenode(node, Pair(Number(node.val//2), Number((node.val+1)//2)))
+            raise ReduceAgainException()
+    else:
+        split(node.lhs)
+        split(node.rhs)
+def reduction(tree):
+    while True:
+        try:
+            explode(tree)
+            split(tree)
+            break
+        except ReduceAgainException:
+            pass
+def parsePair(s):
+    if s[0].isdigit():
+        return Number(int(s[0])), s[1:]
+    else:
+        lhs, s = parsePair(s[1:])
+        rhs, s = parsePair(s[1:])
+        return Pair(lhs, rhs), s[1:]
+
+trees = []
+try:
+    while True:
+        ntree, _ = parsePair(input())
+        trees.append(ntree)
+except EOFError:
+    pass
+
+def magnitude(node):
+    if isinstance(node, Pair):
+        return 3*magnitude(node.lhs) + 2*magnitude(node.rhs)
+    else:
+        return node.val
+def copytree(tree):
+    if isinstance(tree, Pair):
+        return Pair(copytree(tree.lhs), copytree(tree.rhs))
+    else:
+        return Number(tree.val)
+def sumtree(a, b):
+    tree = Pair(copytree(a), copytree(b))
+    reduction(tree)
+    return tree
+print(max(magnitude(sumtree(trees[i], trees[j])) for i in range(len(trees)) for j in range(len(trees)) if i != j))
