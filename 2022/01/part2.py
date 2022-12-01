6dfea742dceb2e0083de9daa380f7b7f57fe0829
Egor <egor@opensrc.club>
2022-12-01 07:08:49 +0200

    2022/01

---
+++ 2022/01/part2.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+lsls = []
+ls = []
+try:
+    while True:
+        line = input()
+        if line == "":
+            lsls.append(ls)
+            ls = []
+        else:
+            ls.append(int(line))
+except EOFError:
+    pass
+lsls.append(ls)
+print(sum(sorted(map(sum, lsls))[-3:]))
