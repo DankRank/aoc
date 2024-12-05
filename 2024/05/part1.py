367e00c92a29979bbd2f2d00b697ac1877dd426f
Egor <egor@opensrc.club>
2024-12-05 08:15:29 +0200

    2024/05

---
+++ 2024/05/part1.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+import sys
+order = []
+updates = []
+try:
+    while True:
+        line = input()
+        if line == '':
+            break
+        order.append(tuple(int(x) for x in line.split('|')))
+    while True:
+        updates.append([int(x) for x in input().split(',')])
+except EOFError:
+    pass
+
+before = {i:set() for i in range(100)}
+for a, b in order:
+    before[b].add(a)
+
+count = 0
+for update in updates:
+    right = set(update)
+    assert(len(right) == len(update)) # this better be unique
+    for i in range(len(update)):
+        right.remove(update[i])
+        if before[update[i]] & right:
+            break # out of order
+    else:
+        count += update[len(update)//2]
+print(count)
---
+++ 2024/05/part2.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+import sys
+order = []
+updates = []
+try:
+    while True:
+        line = input()
+        if line == '':
+            break
+        order.append(tuple(int(x) for x in line.split('|')))
+    while True:
+        updates.append([int(x) for x in input().split(',')])
+except EOFError:
+    pass
+
+before = {i:set() for i in range(100)}
+for a, b in order:
+    before[b].add(a)
+
+count = 0
+for update in updates:
+    corrected = False
+    while True:
+        right = set(update)
+        assert(len(right) == len(update)) # this better be unique
+        for i in range(len(update)):
+            right.remove(update[i])
+            if before[update[i]] & right:
+                corrected = True
+                for j in range(len(update)-1, i, -1):
+                    if update[j] in before[update[i]]:
+                        update[i], update[j] = update[j], update[i]
+                        break
+                break # out of order, try again
+        else:
+            if corrected:
+                count += update[len(update)//2]
+            break
+print(count)
