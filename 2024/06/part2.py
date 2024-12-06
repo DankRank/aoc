c5439fb46fc63f75bb2acf734d45d5485a7f00ce
Egor <egor@opensrc.club>
2024-12-06 07:38:58 +0200

    2024/06: memoization

--- 2024/06/part2.py
+++ 2024/06/part2.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import functools
 import sys
 m = [line.rstrip() for line in sys.stdin]
 w, h = len(m[0]), len(m)
@@ -10,13 +11,21 @@ else:
     raise ValueError('start pos not found')
 dx, dy = 0, -1
 
+def nextstep(x, y, dx, dy, nx, ny):
+    while 0 <= x+dx < w and 0 <= y+dy < h and m[y+dy][x+dx] != '#' and (x+dx, y+dy) != (nx, ny):
+        x += dx
+        y += dy
+    if not (0 <= x+dx < w and 0 <= y+dy < h):
+        return x+dx, y+dy, 0, 0
+    return x, y, -dy, dx
+nextstepcache = functools.cache(nextstep)
 def checkobs(x, y, dx, dy, nx, ny):
     visited = {(x, y, dx, dy)}
     while 0 <= x+dx < w and 0 <= y+dy < h:
-        while m[y+dy][x+dx] == '#' or x+dx == nx and y+dy == ny:
-            dx, dy = -dy, dx
-        x += dx
-        y += dy
+        if x != nx and y != ny:
+            x, y, dx, dy = nextstepcache(x, y, dx, dy, None, None)
+        else:
+            x, y, dx, dy = nextstep(x, y, dx, dy, nx, ny)
         if (x, y, dx, dy) in visited:
             return True
         visited.add((x, y, dx, dy))
