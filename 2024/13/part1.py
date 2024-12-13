2cf688a9b5b8ed1333524ad45b67e7059c523a66
Egor <egor@opensrc.club>
2024-12-13 08:34:07 +0200

    2024/13: double test

--- 2024/13/part1.py
+++ 2024/13/part1.py
@@ -1,37 +1,31 @@
 #!/usr/bin/env python3
-import functools
-import sys
-sys.setrecursionlimit(10000)
+from fractions import Fraction
 m = []
 try:
     while True:
         ax, ay = (int(x.split('+')[1]) for x in input().split(': ')[1].split(', '))
         bx, by = (int(x.split('+')[1]) for x in input().split(': ')[1].split(', '))
         px, py = (int(x.split('=')[1]) for x in input().split(': ')[1].split(', '))
-        m.append((ax, ay, bx, by, px, py))
+        m.append([ax, ay, bx, by, px, py])
         input()
 except EOFError:
     pass
 
-@functools.cache
-def rec(ax, ay, bx, by, px, py):
-    if px == 0 and py == 0:
-        return 0
-    res1, res2 = None, None
-    if ax <= px and ay <= py:
-        res1 = rec(ax, ay, bx, by, px-ax, py-ay)
-    if bx <= px and by <= py:
-        res2 = rec(ax, ay, bx, by, px-bx, py-by)
-    if res1 is None:
-        if res2 is None:
-            return None
-        else:
-            return 1+res2
-    else:
-        if res2 is None:
-            return 3+res1
-        else:
-            return min(3+res1, 1+res2)
+def solve():
+    count = 0
+    for ax, ay, bx, by, px, py in m:
+        det = ax*by - bx*ay
+        idet = Fraction(1, det)
+        a = px*by*idet + py*-bx*idet
+        b = px*-ay*idet + py*ax*idet
+        if a.denominator != 1 or b.denominator != 1:
+            continue
+        count += 3*a + b
+    return count
 
-print(sum(rec(*params) or 0 for params in m))
+print(solve())
+for line in m:
+    line[4] += 10000000000000
+    line[5] += 10000000000000
+print(solve())
 
