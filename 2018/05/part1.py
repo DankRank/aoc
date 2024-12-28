a6227297c10410afad258d7ef674ba1a3ddc3db4
Egor <egor@opensrc.club>
2024-12-28 13:13:50 +0200

    2018/05: optimize

--- 2018/05/part1.py
+++ 2018/05/part1.py
@@ -3,15 +3,24 @@ line = [ord(x) for x in input()]
 
 def reduce(line):
     changed = True
+    l = len(line)
     while changed:
         changed = False
         i = 0
-        while i < len(line)-1:
-            if line[i]^line[i+1] == 0x20:
-                del line[i:i+2]
+        j = 0
+        while i < l:
+            if i < l-1 and line[i]^line[i+1] == 0x20:
+                i += 2
+                while j and i < l and line[j-1]^line[i] == 0x20:
+                    i += 1
+                    j -= 1
                 changed = True
             else:
+                line[j] = line[i]
                 i += 1
+                j += 1
+        l = j
+    del line[l:]
     return line
 
 print(len(reduce(line.copy())))
